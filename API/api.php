40664e869007330f842278e7fe822badb1d93e65
Mohamed Sheikh <mysheikh08@gmail.com>
2017-11-09 10:09:50 -0500

    edited the api file

--- API/api.php
+++ API/api.php
@@ -1,14 +1,19 @@
 
 <?php
+
     $request = 'http://api.openweathermap.org/data/2.5/forecast?id=4140963&APPID=6dda62a111242c0a8a93428164914861';
     $response  = file_get_contents($request);
     $jsonobj  = json_decode($response);
+    echo '<pre>';
     print_r($jsonobj);
-?>
+    echo '</pre> <br/>';
 
-<?php
+
     $request = 'http://api.openweathermap.org/data/2.5/weather?zip=22180,us&appid=6dda62a111242c0a8a93428164914861';
     $response  = file_get_contents($request);
     $jsonobj  = json_decode($response);
+    echo '<pre>';
     print_r($jsonobj);
+    echo '</pre>';
+    
 ?>
